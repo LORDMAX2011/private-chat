fa5bbaba882bcfed6d46c072e6c22ad7be757914
LORDMAX2011 <chimeziechibuikem400@gmail.com>
2026-08-27 17:45:23 +0100

    Initial commit

---
+++ const express = require('express');.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on('send-message', ({ roomId, message, sender }) => {
+    socket.to(roomId).emit('receive-message', { message, sender });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
